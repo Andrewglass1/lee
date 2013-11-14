b07bb407204bb7096b963102cb3d1dd167102ab9
Andrew Glass <andy.glass@livingsocial.com>
2013-11-13 21:31:27 -0500

    implement project new and show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,5 +4,22 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
+		@project = Project.find_by_slug(params[:slug])
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+  	@project = Project.new(params[:project].permit(:name, :slug, :image_1, :image_2, :description))
+ 
+	  if @project.save
+	    redirect_to new_project_path
+	    flash[:notice] = "You have successfully added a project <a href='/projects/#{@project.slug}'>#{@project.name}</a>"
+	  else
+	    render 'new'
+	    flash[:notice] = "There was an issue in adding the project"
+	  end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Lee::Application.routes.draw do
   root 'info#index'
 
-  resources :projects, :only => [:index, :show]
+  resources :projects, :only => [:index, :new, :create]
+  get "projects/:slug" => "projects#show"
+
   resources :info, :only => [:index]
   resources :teaching, :only => [:index]
   resources :shop, :only => [:index]
