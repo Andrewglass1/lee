675f5dbd9c4204143834dca1ccdee50a7998fdd6
Andrew Glass <andy.glass@livingsocial.com>
2013-11-30 22:59:10 -0500

    make last and prev work

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,22 @@
 class Project < ActiveRecord::Base
 
 	def next_proj_link
-		"/projects/#{Project.first.slug}"
+		"/projects/#{next_proj.slug}"
 	end
 
 	def prev_proj_link
-		"/projects/#{Project.first.slug}"
+		"/projects/#{prev_proj.slug}"
 	end
+
+  private
+
+  def next_proj
+    p = Project.first(:conditions => ['id > ?', id], :order => 'id DESC')
+    p || Project.first
+  end
+
+  def prev_proj
+    Project.first(:conditions => ['id > ?', id], :order => 'id ASC')
+    p || Project.last
+  end
 end
