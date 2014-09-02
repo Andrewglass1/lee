45fd3b117baba3146f90a1a3bdef30704da9a629
Andrew Glass <andrewglass1@gmail.com>
2014-09-02 11:42:09 -0400

    archive old teaching content

--- app/controllers/teaching_controller.rb
+++ app/controllers/teaching_controller.rb
@@ -3,15 +3,15 @@ class TeachingController < ApplicationController
 	before_filter :redirect_unless_admin, :only => [:edit, :update]
 
 	def index
-		@teaching_content = TeachingContent.first
+		@teaching_contents = TeachingContent.all.reverse
 	end
 
 	def edit
-		@teaching_content = TeachingContent.first
+		@teaching_content = TeachingContent.last
 	end
 
 	def update
-		@teaching_content = TeachingContent.first
+		@teaching_content = TeachingContent.last
 		if @teaching_content.update(teaching_content_params)
 			redirect_to teaching_index_path
 		else
